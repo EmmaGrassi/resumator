69b52560cc7b44e0c8d82353c278709bcd229c4c
Tonino Catapano <info@toninocatapano.com>
2015-11-17 12:19:51 +0100

    refactoring App + new Resumator(launcher)App

--- src/main/java/com/sytac/resumator/App.java
+++ src/main/java/com/sytac/resumator/App.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -30,31 +29,30 @@ import com.google.gdata.data.spreadsheet.ListFeed;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
 import com.google.gdata.data.spreadsheet.WorksheetEntry;
-import com.google.gdata.util.AuthenticationException;
 import com.google.gdata.util.ServiceException;
 
 public class App {
 
-	private static final String SERVICE_NAME = "resumator-service";
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
-
+	
+	
+	private static final String NAME_COLUMN = "name";
+	private static final String SURNAME_COLUMN = "surname";
+	private static final String ADDRESS_COLUMN = "address";
+	private static final String EXP1_COLUMN = "exp1";
+	private static final String EXP2_COLUMN = "exp2";
+	private static final String EXP3_COLUMN = "exp3";
+	
+	private static final String SERVICE_NAME = "resumator-service";
 	private static final File DATA_STORE_DIR = new File(System.getProperty("user.home"), ".credentials/resumator");
-
-	private static final List<String> SCOPES = Arrays.asList("https://spreadsheets.google.com/feeds",
-			"https://docs.google.com/feeds");
-
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-	private static HttpTransport HTTP_TRANSPORT;
-
+	private static final List<String> SCOPES = Arrays.asList("https://spreadsheets.google.com/feeds","https://docs.google.com/feeds");
 	private static final String CLIENT_TOKENS_PATH = "/client_secret.json";
-
 	private static final String SHEET_FEED_URL = "https://spreadsheets.google.com/feeds/spreadsheets/private/full";
-
-	public static URL SPREADSHEET_FEED_URL;
+	
+	private static URL SPREADSHEET_FEED_URL;
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static FileDataStoreFactory DATA_STORE_FACTORY;
+	private static HttpTransport HTTP_TRANSPORT;
 
 	static {
 		try {
@@ -66,26 +64,102 @@ public class App {
 			System.exit(1);
 		}
 	}
-
-	public static JsonFactory getJsonFactory() {
-		return JSON_FACTORY;
+	
+	
+	private SpreadsheetService service;
+	private SpreadsheetFeed feed;
+	private List<SpreadsheetEntry> spreadsheetsList;
+	private SpreadsheetEntry sheet;
+	private List<WorksheetEntry> worksheets;
+	private String workingSheet;
+	
+	
+	public SpreadsheetService getService() {
+		return service;
 	}
 
-	public static List<String> getScopesList() {
-		return SCOPES;
+	public void setService(SpreadsheetService service) {
+		this.service = service;
 	}
 
-	public static File getDataStoreDir() {
-		return DATA_STORE_DIR;
+	public SpreadsheetFeed getFeed() {
+		return feed;
 	}
 
-	/**
-	 * Creates an authorized Credential object.
-	 * 
-	 * @return an authorized Credential object.
-	 * @throws IOException
-	 */
-	public static Credential authorize() throws IOException {
+	public void setFeed(SpreadsheetFeed feed) {
+		this.feed = feed;
+	}
+
+	public List<SpreadsheetEntry> getSpreadsheetsList() {
+		return spreadsheetsList;
+	}
+
+	public void setSpreadsheetsList(List<SpreadsheetEntry> spreadsheetsList) {
+		this.spreadsheetsList = spreadsheetsList;
+	}
+
+	public SpreadsheetEntry getSheet() {
+		return sheet;
+	}
+
+	public void setSheet(SpreadsheetEntry sheet) {
+		this.sheet = sheet;
+	}
+
+	public List<WorksheetEntry> getWorksheets() {
+		return worksheets;
+	}
+
+	public void setWorksheets(List<WorksheetEntry> worksheets) {
+		this.worksheets = worksheets;
+	}
+
+	public String getWorkingSheetName() {
+		return workingSheet;
+	}
+
+	public void setWorkingSheet(String sheetName) throws IOException, ServiceException {
+		sheet = findSpreadSheet(sheetName); //throw exception if sheet not found
+		worksheets = sheet.getWorksheets();
+		this.workingSheet = sheetName;
+	}
+	
+	
+
+	public void init() throws IOException, ServiceException{
+		service = initializeAndGetSsService();
+		feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
+		spreadsheetsList = feed.getEntries();
+	}
+	
+	public String findResumeeInSpreadSheet(String name, int sheetPageNr) throws IOException, ServiceException{
+		WorksheetEntry page = getWorkSheetEntryByPosition(sheetPageNr);
+		ListFeed listFeed = service.getFeed(page.getListFeedUrl(), ListFeed.class);
+		for (ListEntry row : listFeed.getEntries()) { // O(n^2)
+			if(row.getCustomElements().getValue(NAME_COLUMN).equalsIgnoreCase(name)){
+				return row.getPlainTextContent();
+			}
+		}
+		return null;
+	}
+	
+
+	public String insertGuy(int sheetPageNr, String name, String surname, String address, String exp1) throws IOException, ServiceException{
+		WorksheetEntry page = getWorkSheetEntryByPosition(sheetPageNr);
+		URL listFeedUrl = page.getListFeedUrl();
+		ListEntry row = new ListEntry();
+		row.getCustomElements().setValueLocal(NAME_COLUMN, name);
+		row.getCustomElements().setValueLocal(SURNAME_COLUMN, surname);
+		row.getCustomElements().setValueLocal(ADDRESS_COLUMN, address);
+		row.getCustomElements().setValueLocal(EXP1_COLUMN, exp1);
+		row = service.insert(listFeedUrl, row);
+		return row.getPlainTextContent();
+		
+	}
+
+
+
+	Credential authorize() throws IOException {
 		InputStream in = App.class.getResourceAsStream(CLIENT_TOKENS_PATH);
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
@@ -95,17 +169,16 @@ public class App {
 		return credential;
 	}
 
-	public static SpreadsheetService initializeAndGetSsService() throws IOException {
+	SpreadsheetService initializeAndGetSsService() throws IOException {
 		final SpreadsheetService service = new SpreadsheetService(SERVICE_NAME);
 		service.setProtocolVersion(SpreadsheetService.Versions.V3);
 		service.setOAuth2Credentials(authorize());
 		return service;
 	}
-
-	public static SpreadsheetEntry findSpreadSheet(List<SpreadsheetEntry> spreadsheets, String sheetName)
-			throws NoSuchElementException {
+	
+	SpreadsheetEntry findSpreadSheet(String sheetName){
 		SpreadsheetEntry dbSheet = null;
-		final Iterator<SpreadsheetEntry> iterator = spreadsheets.iterator();
+		final Iterator<SpreadsheetEntry> iterator = spreadsheetsList.iterator();
 		while (iterator.hasNext()) {
 			dbSheet = iterator.next();
 			if (dbSheet.getTitle().getPlainText().trim().equals(sheetName))
@@ -113,42 +186,9 @@ public class App {
 		}
 		throw new NoSuchElementException("SpreadsheetEntry " + sheetName + " not found");
 	}
-
-	public static void main(String[] args)
-			throws AuthenticationException, MalformedURLException, IOException, ServiceException {
-
-		final SpreadsheetService service = initializeAndGetSsService();
-		final SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
-		final List<SpreadsheetEntry> spreadsheetsList = feed.getEntries();
-		
-		final SpreadsheetEntry sheet = findSpreadSheet(spreadsheetsList, "Test");
-		LOGGER.info("Sheet name found: " + sheet.getTitle().getPlainText());
-		final List<WorksheetEntry> worksheets = sheet.getWorksheets();
-
-		for (WorksheetEntry worksheet : worksheets) {
-			String title = worksheet.getTitle().getPlainText();
-			int rowCount = worksheet.getRowCount();
-			int colCount = worksheet.getColCount();
-			URL listFeedUrl = worksheet.getListFeedUrl();
-			ListFeed listFeed = service.getFeed(listFeedUrl, ListFeed.class);
-			for (ListEntry row : listFeed.getEntries()) {
-				System.out.print(row.getPlainTextContent() + "\n");
-				/*
-				 * for (String tag : row.getCustomElements().getTags()) {
-				 * System.out.print(row.getCustomElements().getValue(tag) +
-				 * "\n"); }
-				 */
-				// LOGGER.info();
-			}
-			ListEntry row = new ListEntry();
-			row.getCustomElements().setValueLocal("name", "Joe");
-			row.getCustomElements().setValueLocal("surname", "Smith");
-			row.getCustomElements().setValueLocal("address", "2011LS");
-			row.getCustomElements().setValueLocal("exp1", "Sytac yep");
-			row = service.insert(listFeedUrl, row);
-			// LOGGER.info("\t" + title + "- rows:" + rowCount + " cols: " +
-			// colCount);
-		}
+	
+	private WorksheetEntry getWorkSheetEntryByPosition(int position){
+		return worksheets.get(position);
 	}
 
 }

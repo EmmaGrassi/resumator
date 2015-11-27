61f34b07b303881b53480aa83634c34d0f46cadc
Carlo Sciolla <skuro@skuro.tk>
2015-11-27 17:39:24 +0100

    Renamed rill into resumator. You know, branding :-)

--- src/sql/events-schema.sql
+++ src/sql/events-schema.sql
@@ -1,8 +1,8 @@
--- Shamelessly taken from https://github.com/rill-event-sourcing/rill
+-- Shamelessly taken from https://github.com/resumator-event-sourcing/rill
 
-CREATE SEQUENCE rill_events_insert_order_seq;
+CREATE SEQUENCE resumator_events_insert_order_seq;
 
-CREATE TABLE rill_events (
+CREATE TABLE resumator_events (
        event_id VARCHAR(200) UNIQUE NOT NULL,
        stream_id VARCHAR(512) NOT NULL,
        insert_order BIGINT UNIQUE,
@@ -13,23 +13,23 @@ CREATE TABLE rill_events (
        UNIQUE(stream_id, stream_order)
 );
 
-CREATE INDEX stream_id_index ON rill_events (stream_id);
-CREATE INDEX event_type_index ON rill_events (event_type);
+CREATE INDEX stream_id_index ON resumator_events (stream_id);
+CREATE INDEX event_type_index ON resumator_events (event_type);
 
-ALTER SEQUENCE rill_events_insert_order_seq OWNED BY rill_events.insert_order;
+ALTER SEQUENCE resumator_events_insert_order_seq OWNED BY resumator_events.insert_order;
 
-CREATE FUNCTION rill_set_insert_order() RETURNS trigger AS $$
+CREATE FUNCTION resumator_set_insert_order() RETURNS trigger AS $$
        DECLARE
         event RECORD;
        BEGIN
           -- ensure that insert order is generated in order of *visibility*
-          PERFORM pg_advisory_xact_lock(3333, 'rill_events'::regclass::oid::integer);
-          FOR event IN SELECT stream_id, stream_order FROM rill_events WHERE insert_order IS NULL ORDER BY stream_id ASC, stream_order ASC LOOP
-            UPDATE rill_events SET insert_order = nextval('rill_events_insert_order_seq') WHERE stream_id = event.stream_id AND stream_order = event.stream_order;
+          PERFORM pg_advisory_xact_lock(3333, 'resumator_events'::regclass::oid::integer);
+          FOR event IN SELECT stream_id, stream_order FROM resumator_events WHERE insert_order IS NULL ORDER BY stream_id ASC, stream_order ASC LOOP
+            UPDATE resumator_events SET insert_order = nextval('resumator_events_insert_order_seq') WHERE stream_id = event.stream_id AND stream_order = event.stream_order;
           END LOOP;
           RETURN NULL;
        END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER rill_set_insert_order AFTER INSERT ON rill_events
-       FOR EACH STATEMENT EXECUTE PROCEDURE rill_set_insert_order();
+CREATE TRIGGER resumator_set_insert_order AFTER INSERT ON resumator_events
+       FOR EACH STATEMENT EXECUTE PROCEDURE resumator_set_insert_order();

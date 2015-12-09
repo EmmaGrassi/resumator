11e1fac6a619a0979f92c0ac71c5d6040d5ed4d5
Carlo Sciolla <carlo.sciolla@gmail.com>
2015-12-10 00:12:36 +0100

    Starting to implement sign out; added expiry to the cookie

--- resumator-ui/src/js/login.js
+++ resumator-ui/src/js/login.js
@@ -3,14 +3,38 @@
  *
  * @author Carlo Sciolla
  */
+function setCookie(token){
+    function inOneHour() {
+        var now = new Date();
+        var oneHour = now.getTime();
+        oneHour += 3600 * 1000;
+        now.setTime(oneHour);
+        return now;
+    }
+
+    var expiry = inOneHour(token);
+    document.cookie =
+        'resumatorJWT=' + token +
+        '; expires=' + expiry.toUTCString() +
+        '; path=/';
+}
+
 function onSignIn(googleUser) {
     var idtoken = googleUser.getAuthResponse().id_token;
-    document.cookie = "resumatorJWT=" + idtoken;
+    setCookie(idtoken);
 
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-    navigationView.model.set("icon", profile.getImageUrl());
+    navigationView.model.set('icon', profile.getImageUrl());
+    navigationView.model.set('profile', profile);
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
 }

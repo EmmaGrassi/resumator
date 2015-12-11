061a3d6969bb56a1db70f12c556b1b6c8aa1f7e1
Carlo Sciolla <carlo.sciolla@gmail.com>
2015-12-11 17:19:47 +0100

    Added a method to register organizations

--- resumator-service/src/main/java/io/sytac/resumator/store/InMemoryOrganizationRepository.java
+++ resumator-service/src/main/java/io/sytac/resumator/store/InMemoryOrganizationRepository.java
@@ -1,6 +1,8 @@
 package io.sytac.resumator.store;
 
 import io.sytac.resumator.model.Organization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,15 +15,24 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class InMemoryOrganizationRepository implements OrganizationRepository {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryOrganizationRepository.class);
+
     private final ConcurrentHashMap<String, Organization> organizations = new ConcurrentHashMap<>();
 
     @Override
-    public Optional<Organization> get(String id) {
+    public Optional<Organization> get(final String id) {
         return Optional.ofNullable(organizations.get(id));
     }
 
     @Override
-    public Optional<Organization> fromDomain(String domain) {
+    public Optional<Organization> fromDomain(final String domain) {
         return organizations.values().stream().filter(org -> org.getDomain().equals(domain)).findFirst();
+    }
+
+    @Override
+    public Organization register(final Organization org) {
+        Optional.ofNullable(organizations.putIfAbsent(org.getId(), org))
+                .ifPresent(old -> LOGGER.warn("Replacing existing organization: {}", old));
+        return org;
     }
 }

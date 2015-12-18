8d783100355bb784f2d80bcc262e1c39c4ab2f37
Carlo Sciolla <carlo.sciolla@gmail.com>
2015-12-18 19:09:25 +0100

    Switching to sync event bus to ease development for the time being

--- resumator-service/src/main/java/io/sytac/resumator/events/LocalEventPublisher.java
+++ resumator-service/src/main/java/io/sytac/resumator/events/LocalEventPublisher.java
@@ -1,7 +1,7 @@
 package io.sytac.resumator.events;
 
 import com.google.common.base.Charsets;
-import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
 import io.sytac.resumator.Configuration;
 import io.sytac.resumator.http.command.model.NewEmployeeCommand;
 import io.sytac.resumator.model.Event;
@@ -11,12 +11,8 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import java.sql.Timestamp;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static io.sytac.resumator.ConfigurationEntries.LOG_TAG;
-import static io.sytac.resumator.ConfigurationEntries.THREAD_POOL_SIZE;
 
 /**
  * Publishes event within the local JVM
@@ -28,28 +24,12 @@ public class LocalEventPublisher implements EventPublisher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LocalEventPublisher.class);
 
-    private final AsyncEventBus eventBus;
+    private final EventBus eventBus;
 
     @Inject
     public LocalEventPublisher(final Configuration configuration) {
         final String id = configuration.getProperty(LOG_TAG).orElse("resumator");
-        final ExecutorService executor = createExecutorService(configuration);
-        eventBus = new AsyncEventBus(id, executor);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                try {
-                    executor.awaitTermination(5, TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
-                    LOGGER.error("Shutdown of the event system wasn't clean, there's potential for data loss");
-                }
-            }
-        });
-    }
-
-    protected ExecutorService createExecutorService(final Configuration configuration) {
-        final Integer concurrency = configuration.getIntegerProperty(THREAD_POOL_SIZE).orElse(1);
-        return Executors.newFixedThreadPool(concurrency);
+        eventBus = new EventBus(id);
     }
 
     @Override

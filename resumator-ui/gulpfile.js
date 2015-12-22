0a38f9caa8779fa03a437dc3eeb595be446dd487
Tom Wieland <tom.wieland@gmail.com>
2015-12-22 11:22:22 +0000

    disabled tasks that are not used

--- resumator-ui/gulpfile.js
+++ resumator-ui/gulpfile.js
@@ -1,9 +1,28 @@
-var gulp = require('gulp');
-
 // Enable ES6 code from here.
 require('babel-register')({
   // TODO: Take these options from a shared configuration JSON file somewhere.
   presets: [ 'es2015', 'react', 'stage-0' ]
 });
 
-require('id-builder')(gulp);
+var gulp = require('gulp');
+var idBuilder = require('id-builder');
+
+idBuilder(gulp, {
+  tasks: {
+    runServer: {
+      enabled: false
+    },
+
+    compileBrowserify: {
+      enabled: false
+    },
+
+    runTests: {
+      enabled: false
+    },
+
+    watchTests: {
+      enabled: false
+    }
+  }
+});

084036863dc74d09dc7cec14ef03ff8da1b00496
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-03-09 15:05:43 +0100

    Methodologies are not required

--- resumator-ui/src/js/components/shared/form1/experience.js
+++ resumator-ui/src/js/components/shared/form1/experience.js
@@ -70,7 +70,7 @@ class ExperienceForm extends FormComponent {
   }
 
   renderEndDate() {
-    return this.getInput('endDate', 'date', true);
+    return this.getInput('endDate', 'date', false);
   }
 
   renderShortDescription() {
@@ -82,7 +82,7 @@ class ExperienceForm extends FormComponent {
   }
 
   renderMethodologies() {
-    return this.getInput('methodologies', null, true, "Separate values with commas, e.g.: SCRUM, RUP, ...");
+    return this.getInput('methodologies', null, false, "Separate values with commas, e.g.: SCRUM, RUP, ...");
   }
 
   render() {

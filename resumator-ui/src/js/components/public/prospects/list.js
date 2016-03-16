397496b76b1accba666c962b1beada81f2529b24
Ramon Gebben <ramon@ra-ge.net>
2016-03-16 13:43:10 +0100

    added visible keys to other types as wel

--- resumator-ui/src/js/components/public/prospects/list.js
+++ resumator-ui/src/js/components/public/prospects/list.js
@@ -79,6 +79,7 @@ class List extends React.Component {
       handleOpen={this.handleRowButtonClick.bind(this)}
       handleEdit={this.handleEditButtonClick.bind(this)}
       handleRemove={this.handleRemoveButtonClick.bind(this)}
+      visibleKeys={['fullName', 'currentClient', 'role', 'phone']}
     />);
   }
 

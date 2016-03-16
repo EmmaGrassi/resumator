3bffa04d808877d0592e45f5504af3d1f755d3ee
Ramon Gebben <ramon@ra-ge.net>
2016-03-16 15:10:19 +0100

    added search

--- resumator-ui/src/js/components/shared/table.js
+++ resumator-ui/src/js/components/shared/table.js
@@ -6,6 +6,8 @@ import labelize from '../../helpers/labelize'
 import {
   Button,
   Glyphicon,
+  Input,
+  Row, Col
 } from 'react-bootstrap';
 
 
@@ -26,7 +28,8 @@ class SortHeaderCell extends React.Component {
   }
 
   render() {
-    const {sortDir, children, ...props} = this.props;
+    // eslint-disable-next-line
+    const { sortDir, children, ...props } = this.props;
     return (
       <Cell {...props} onClick={this._onSortChange} className="sortHeader">
         {children} {sortDir ? (sortDir === SortTypes.DESC ? '↓' : '↑') : ''}
@@ -54,17 +57,18 @@ class ReactTable extends React.Component {
     super(props);
 
     this.state = {
-      sortedDataList: this.props.data,
+      dataList: this.props.data,
       colSortDirection: {},
+      activeSearchKey: this.props.visibleKeys[0],
     };
   }
 
   componentWillReceiveProps(nextProps){
-    this.setState({sortedDataList: nextProps.data});
+    this.setState({dataList: nextProps.data});
   }
 
   _onSortChange(columnKey, sortDir) {
-      var sortIndexes = this.state.sortedDataList.slice();
+      var sortIndexes = this.state.dataList.slice();
       sortIndexes.sort((a, b) => {
         const valA = a[columnKey];
         const valB = b[columnKey];
@@ -80,15 +84,41 @@ class ReactTable extends React.Component {
       });
 
       this.setState({
-        sortedDataList: sortIndexes,
+        dataList: sortIndexes,
         colSortDirection: {
           [columnKey]: sortDir,
         },
       });
     }
 
+  changeActiveSearchKey(e){
+    console.log('Search key changed', this.refs.activeSearchKey.getValue());
+    this.setState({activeSearchKey: this.refs.activeSearchKey.getValue()})
+  }
+
+  handleSearchChange(e){
+    if( e.target.value !== '' ){
+      this.applyFilter(e.target.value);
+    }else {
+      this.resetFilter();
+    }
+  }
+
+  applyFilter(query){
+    const newList = this.props.data
+      .filter(row => row[this.state.activeSearchKey].includes(query));
+    this.setState({dataList: newList});
+  }
+
+  resetFilter(){
+    this.setState({dataList: this.props.data});
+  }
+
   render() {
-    const {sortedDataList, colSortDirection} = this.state;
+    const {dataList, colSortDirection} = this.state;
+
+    const tableWidth = 940;
+    const celWidth = tableWidth / (this.props.visibleKeys.length + 1);
 
     const colums = this.props.visibleKeys.map((key, i)=> {
       return (<Column
@@ -103,46 +133,70 @@ class ReactTable extends React.Component {
         }
         cell={props => (
           <Cell {...props}>
-            {this.state.sortedDataList[props.rowIndex][key]}
+            {this.state.dataList[props.rowIndex][key]}
           </Cell>
         )}
-        width={200}
+        width={celWidth}
       />);
     });
 
     return (
-      <Table
-        rowsCount={this.state.sortedDataList.length}
-        rowHeight={50}
-        headerHeight={50}
-        width={1000}
-        height={500}
-      >
-        {colums}
-        <Column
-          header={<Cell>Actions</Cell>}
-          cell={props => (
-            <Cell {...props}>
-              <Button onClick={this.props.handleOpen.bind(this, this.state.sortedDataList[props.rowIndex].email)}
-              >
-                <Glyphicon glyph="eye-open" />
-              </Button>
-              &nbsp;
-              <Button onClick={this.props.handleEdit.bind(this, this.state.sortedDataList[props.rowIndex].email)}
-              >
-                <Glyphicon glyph="pencil" />
-              </Button>
-              &nbsp;
-              <Button onClick={this.props.handleRemove.bind(this, this.state.sortedDataList[props.rowIndex].email)}
-                bsStyle="danger"
-              >
-                <Glyphicon glyph="trash" />
-              </Button>
-            </Cell>
-          )}
-          width={200}
-        />
-      </Table>
+      <div className="table-container">
+        <Row>
+          <Col xs={2}>
+            <Input
+              ref='activeSearchKey'
+              type='select'
+              placeholder='Active search key'
+              onChange={this.changeActiveSearchKey.bind(this)}
+              value={this.state.activeSearchKey}
+            >
+              {this.props.visibleKeys.map((key, i) => <option key={i} value={key}>{labelize(key)}</option>)}
+            </Input>
+          </Col>
+          <Col xs={10}>
+            <Input
+              type="search"
+              placeholder={`Search by ${labelize(this.state.activeSearchKey, null)}`}
+              ref="keySearch"
+              onChange={this.handleSearchChange.bind(this)}
+            />
+          </Col>
+        </Row>
+
+        <Table
+          rowsCount={this.state.dataList.length}
+          rowHeight={50}
+          headerHeight={50}
+          width={tableWidth}
+          height={500}
+        >
+          {colums}
+          <Column
+            header={<Cell>Actions</Cell>}
+            cell={props => (
+              <Cell {...props}>
+                <Button onClick={this.props.handleOpen.bind(this, this.state.dataList[props.rowIndex].email)}
+                >
+                  <Glyphicon glyph="eye-open" />
+                </Button>
+                &nbsp;
+                <Button onClick={this.props.handleEdit.bind(this, this.state.dataList[props.rowIndex].email)}
+                >
+                  <Glyphicon glyph="pencil" />
+                </Button>
+                &nbsp;
+                <Button onClick={this.props.handleRemove.bind(this, this.state.dataList[props.rowIndex].email)}
+                  bsStyle="danger"
+                >
+                  <Glyphicon glyph="trash" />
+                </Button>
+              </Cell>
+            )}
+            width={celWidth}
+          />
+        </Table>
+      </div>
     );
   }
 }
